45a172cb656000cc0809d88ace18bf08d9e1761c
varFox <31412438+varFox@users.noreply.github.com>
2019-05-16 16:19:50 +0300

    14

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $('.for__overlay').on('click', () => {
+    $('.overlay').animate(
+      {
+        opacity: 'toggle'
+      }, 1500
+    );
+    $('.modal').fadeIn().animate(
+      {
+        top: '0px',
+        width: '40%'
+      }, 1500
+    );
+    
+  });
+  $('.close').on('click', () => {
+    $('.overlay').animate(
+      {
+        opacity: 'toggle'
+      }, 1000
+    );
+    $('.modal').animate(
+      {
+        top: '-600px',
+        width: '20%'
+      }, 1000
+    ).fadeOut();
+  });
+
+  $("#submit").on('click', (e) => {
+    e.preventDefault();
+    let name = $('.contactform_name').val();
+        phone = $('.contactform_phone').val(),
+        mail = $('.contactform_mail').val(),
+        text = $('textarea').val();
+    $.ajax({
+     url: "server.php",
+     type: "POST",
+     dataType: "text",
+     data: ("name=" + name + "&phone=" + phone + "&mail=" + mail + "&text=" + text),
+     success: function(data){
+      $("#block").text(data);
+     }
+     });
+    });
+});
